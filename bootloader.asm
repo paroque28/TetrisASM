74560d5021bf6027db51d736419bf24e60b8d267
DiSoJi <diegosolisj@gmail.com>
2019-03-12 15:25:24 -0600

    Fixed Error

--- bootloader.asm
+++ bootloader.asm
@@ -1,5 +1,17 @@
-bits 16			; Tell nasm that we are running in real mode
-org 0x7C00    		; Bootloader starts at physical address 0x07c00
+[BITS 16]      ; Tell nasm that we are running in real mode
+[ORG 0x7C00]   ; Bootloader starts at physical address 0x07c00
+
+
+jmp word 0x000:flush ;#FAR jump so that you set CS to 0. (the first argument is what segment to jump to. The argument(after the `:`) is what offset to jump to)
+;# Without the far jmp, CS could be `0x7C0` or something similar, which will means that where the assembler thinks the code is loaded and where your computer loaded the code is different. Which in turn messes up the absolute addresses of labels.
+flush: ;#We go to here, but we do it ABSOLUTE. So with this, we can reset the segment and offset of where our code is loaded.
+	mov BP,0x0000 ;#use BP as a temp register
+	mov DS,BP ;#can not assign segment registers a literal number. You have to assign to a register first.
+	mov ES,BP ;#do the same here too
+	;#without setting DS and ES, they could have been loaded with the old 0x7C0, which would mess up absolute address calculations for data.
+	jmp 0x0000:init
+;bits 16			
+;org 0x7C00    
 
 init:
 	XOR AX, AX		; Reset value of register
@@ -17,7 +29,7 @@ init:
 
     	MOV AX, 0x1000		; When we read the sector, we are going to read address 0x1000
     	MOV ES, AX     		; Set ES with 0x1000
-			mov si, msg             ; SI now points to our message
+		mov si, msg             ; SI now points to our message
 	mov ah, 0x0E            ; Indicate BIOS we're going to print chars
 .loop	lodsb                   ; Loads SI into AL and increments SI [next char]
 	or al, al               ; Checks if the end of the string
